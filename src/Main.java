a8659b00fc9debd4c80155a368ea3064078bc3d5
sokny2023 <142331738+sokny2023@users.noreply.github.com>
2024-05-15 09:18:33 +0700

    Update Main.java error again

--- src/Main.java
+++ src/Main.java
@@ -2,14 +2,14 @@ import demo.Customer;
 import demo.Sale;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) 
         Customer customer1 = new Customer("John Doe", "Premium");
         Sale sale1 = new Sale(customer1, "2024-02-21", 600.0, 300.0);
         sale1.displayInfo();
 
         Customer customer2 = new Customer("Lovi Hola", "Gold");
         Sale sale2 = new Sale(customer2, "2024-02-21", 230.0, 180.0);
-        sale2.displayInfo();
+        sale.displayInfo();
 
         Customer customer3 = new Customer("Hong Lina", "Silver");
         Sale sale3 = new Sale(customer3, "2024-02-22", 240.0, 190.0);
